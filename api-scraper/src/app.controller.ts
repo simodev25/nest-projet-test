1415200582ea450f80bf913f2e76d6e47ff343b2
mbensass <simo25@gmail.com>
2020-02-20 22:38:48 +0100

    add api-scraper

--- api-scraper/src/app.controller.ts
+++ api-scraper/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,@Inject('ClientProxyFactory') private readonly client: ClientProxy) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    const pattern = { cmd: 'getProducts' };
+
+    return this.client.send<any>(pattern, { });
+
   }
 }
--- api-scraper/src/app.module.ts
+++ api-scraper/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,{
+    provide: 'ClientProxyFactory',
+    useFactory: () => {
+
+      return ClientProxyFactory.create({
+        transport: Transport.TCP,
+
+      });
+    },
+  },],
 })
 export class AppModule {}
